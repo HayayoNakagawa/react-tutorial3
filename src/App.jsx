fc2b3dcab69556223a8fa3e3c081ea044a5a5750
John Doe <johndoe@example.com>
2024-10-21 10:06:08 +0900

    内容の変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
 import { useState } from "react";
 
 const img = [
-    {src:"images/pic1.jpg",alt:"Closeup of a human eye"},
-    {src:"images/pic2.jpg",alt:"Rock that looks like a wave"},
-    {src: "images/pic3.jpg", alt: "Purple and white pansies" },
-    {src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
-    {src: "images/pic5.jpg", alt: "Large moth on a leaf" },
+  { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+  { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+  { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+  { src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+  { src: "images/pic5.jpg", alt: "Large moth on a leaf" },
 ];
 
-
 export default function App() {
-    const [slectendImage, setSelectedImage] = useState(imageArray[0]);
-    const [isDarkened,setIsDarkened] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(img[0]);
+  const [isDarkened, setIsDarkened] = useState(false);
 
-    const handlethumbnailClick = (image) =>{
-        setSelectendImage(image);
-    };
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
+  };
 
-    const toggkeDarken = () => {
-        setIsDarkened((prev) => !prev);
-    };
+  const toggleDarken = () => {
+    setIsDarkened((prev) => !prev);
+  };
 
-    return (
-      <>
-        <h1>Image gallery example</h1>
-        <div className="full-img">
-          <img
-            className="displayed-img"
-            src={selectedImage.src}
-            alt={selectedImage.alt}
-          />
-          <div 
-            className="overlay"
-            style={{
-                backgroundColor: isDarkened ? "rgba(0, 0, 0, 0.5)" : "transparent",
-            }}
-          ></div>
-          <button className="dark" onClick={toggleDarken}>
+  return (
+    <>
+      <h1>Image Gallery Example</h1>
+      <div className="full-img">
+        <img
+          className="displayed-img"
+          src={selectedImage.src}
+          alt={selectedImage.alt}
+        />
+        <div
+          className="overlay"
+          style={{
+            backgroundColor: isDarkened ? "rgba(0, 0, 0, 0.5)" : "transparent",
+          }}
+        ></div>
+        <button className="dark" onClick={toggleDarken}>
           {isDarkened ? "Lighten" : "Darken"}
         </button>
-        </div>
-        <div className="thumb-bar">
-            {ima.map((image,index) =>(
-                <img
-                  key={imdex}
-                  src={image.src}
-                  alt={image.alt}
-                  className="thumb-bar"
-                  onClick={() => handlethumbnailClick(image)}
-                />
-            ))}
-        </div>
-      </>
-    );
-  }
+      </div>
+      <div className="thumb-bar">
+        {img.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            className="thumb-img"
+            onClick={() => handleThumbnailClick(image)}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
